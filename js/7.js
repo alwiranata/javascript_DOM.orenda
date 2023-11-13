731e4e34330c1e092fa967d9301796b611f7fce8
Aldo Wiranata <aldowiranata16@gmail.com>
2023-11-13 15:11:54 +0700

    JS_DOM.orenda

--- js/7.js
+++ js/7.js
@@ -1,12 +1,45 @@
-//p3.dari html
-const p3 = document.querySelector('.p3');
-function changeColor() {
-    p3.style.backgroundColor = 'lightblue';
-}
+// //Event Handler
 
-//p2.dari JS
-function changeColor2() {
-    p2.style.backgroundColor = 'lightblue';
-}
-const p2 = document.querySelector('.p2');
-p2.onclick = changeColor2;
+// //p3. onclick dari html 
+// const p3 = document.querySelector('.p3');
+// function changeColor() {
+//     p3.style.backgroundColor = 'lightblue';
+// }
+
+// //p2. onclick dari JS 
+// function changeColor2() {
+//     p2.style.backgroundColor = 'lightblue';
+// }
+// const p2 = document.querySelector('.p2');
+// p2.onclick = changeColor2;
+
+// //addEventListener
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function () {
+
+//     const ul = document.querySelector('section#b ul');
+//     const liNew = document.createElement('li');
+//     const textLiNew = document.createTextNode('New text');
+//     liNew.appendChild(textLiNew);
+//     liNew.style.color = "tomato";
+//     ul.appendChild(liNew)
+
+// });
+
+
+// p3.onclick = function () {
+//     p3.style.backgroundColor = 'tomato';
+// }
+
+// p3.onclick = function () {
+//     p3.style.color = 'lightblue';
+// }
+const p3 = document.querySelector('.p3');
+
+p3.addEventListener('mouseenter', function () {
+    p3.style.backgroundColor = 'lightblue';
+});
+
+p3.addEventListener('mouseleave', function () {
+    p3.style.backgroundColor = 'white';
+});
